7ec4c24c036b5cce314c0afc5d907fdd4d724e6e
DTOx2RY <dotory1232@gmai.com>
2023-07-05 18:12:57 +0900

    Setting Update(Beta)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -36,6 +36,10 @@ namespace WpfApp1
         private const double TargetWidth = 320;
         private const double TargetHeight = 85;
         static string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1];
+        static bool GameStarted = false;
+
+        static FileInfo op_file = new FileInfo("C:\\Users\\" + username + "\\AppData\\Roaming\\brcl\\option.txt");
+        static DirectoryInfo brcl_file = new DirectoryInfo("C:\\Users\\" + username + "\\AppData\\Roaming\\brcl");
         public MainWindow()
         {
             InitializeComponent();
@@ -45,9 +49,13 @@ namespace WpfApp1
             DwmSetWindowAttribute(hWnd, attribute, ref preference, sizeof(uint));
 
             this.MouseLeftButtonDown += MainWindows_MouseLeftButtonDown;
+            this.Loaded += Load;
+
             CloseButton.MouseLeftButtonDown += Close;
             Play.MouseEnter += MyButton_MouseEnter;
             Play.MouseLeave += MyButton_MouseLeave;
+            CloseButton.MouseEnter += CloseButton_MouseEnter;
+            CloseButton.MouseLeave += CloseButton_MouseLeave;
         }
         public enum DWMWINDOWATTRIBUTE
         {
@@ -73,25 +81,35 @@ namespace WpfApp1
 
         async void StartBRC()
         {
+            Play.IsEnabled = false;
+            AnimateButtonColor("#FF0C001F", 0.5, Play);
+            GameStarted = true;
             try
             {
                 System.Net.ServicePointManager.DefaultConnectionLimit = 256;
+                var loginHandler = JELoginHandlerBuilder.BuildDefault();
+                var accounts = loginHandler.AccountManager.GetAccounts();
+                var session = await loginHandler.Authenticate();
 
                 var path = new MinecraftPath();
 
+                string[] startData = File.ReadAllLines(op_file.FullName);
                 var launcher = new CMLauncher(path);
                 var launchOption = new MLaunchOption
                 {
-                    MaximumRamMb = 1024,
-                    Session = MSession.GetOfflineSession("hello"),
+                    MaximumRamMb = Int32.Parse(startData[0]),
+                    Session = session,
+                    ScreenWidth = Int32.Parse(startData[1]),
+                    ScreenHeight = Int32.Parse(startData[2]),
                 };
+
+
 
                 var process = await launcher.CreateProcessAsync("BRC", launchOption);
                 process.Start();
             }
             catch(Exception e)
             {
-                MessageBox.Show("에러 발생 catch문 진입:" + e);
                 string minecraftVersionsPath = System.IO.Path.Combine(
                     Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
                     ".minecraft",
@@ -127,51 +145,100 @@ namespace WpfApp1
 
                 Console.WriteLine("압축 해제가 완료되었습니다.");
             }
+            Play.IsEnabled = true;
+            AnimateButtonColor("#FF4509A0", 0.5, Play);
+            GameStarted = false;
         }
 
-        async private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-                StartBRC();
+            StartBRC();
         }
         void Close(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        private void CloseButton_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            AnimateButtonColor("#f70000", 0.3, CloseButton);
+        }
+        void CloseButton_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            AnimateButtonColor("#FF4E4E4E", 0.3, CloseButton);
+        }
+
         private void MyButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            AnimateButtonSize(TargetWidth, TargetHeight, 1);
-            AnimateButtonColor("#FF4509A0", 0.5);
+            if(GameStarted == false)
+            {
+                AnimateButtonColor("#FF4509A0", 0.5, Play);
+            }
         }
 
         private void MyButton_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            AnimateButtonSize(DefaultWidth, DefaultHeight, 1);
-            AnimateButtonColor("#FF6B0DF9", 0.5);
-            //asdf
-        }
-
-        private void AnimateButtonSize(double targetWidth, double targetHeight, double durationSeconds)
-        {
-            // Animation code for resizing the button
-        }
-
-        private void AnimateButtonColor(string targetColor, double durationSeconds)
-        {
-            ColorAnimation colorAnimation = new ColorAnimation
+            if(GameStarted == false)
             {
-                To = (Color)ColorConverter.ConvertFromString(targetColor),
-                Duration = TimeSpan.FromSeconds(durationSeconds),
-                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
-            };
+                AnimateButtonColor("#FF6B0DF9", 0.5, Play);
+            }
+        }
 
-            Storyboard storyboard = new Storyboard();
-            storyboard.Children.Add(colorAnimation);
+        private void AnimateButtonColor(string targetColor, double durationSeconds, DependencyObject a)
+        {
+            try
+            {
+                ColorAnimation colorAnimation = new ColorAnimation
+                {
+                    To = (Color)ColorConverter.ConvertFromString(targetColor),
+                    Duration = TimeSpan.FromSeconds(durationSeconds),
+                    EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+                };
 
-            Storyboard.SetTarget(storyboard, Play);
-            Storyboard.SetTargetProperty(colorAnimation, new PropertyPath("(Button.Background).(SolidColorBrush.Color)"));
+                Storyboard storyboard = new Storyboard();
+                storyboard.Children.Add(colorAnimation);
 
-            storyboard.Begin(this);
+                Storyboard.SetTarget(storyboard, a);
+                Storyboard.SetTargetProperty(colorAnimation, new PropertyPath("(Button.Background).(SolidColorBrush.Color)"));
+
+
+                storyboard.Begin(this);
+            } catch
+            {
+                ColorAnimation colorAnimation = new ColorAnimation
+                {
+                    To = (Color)ColorConverter.ConvertFromString(targetColor),
+                    Duration = TimeSpan.FromSeconds(durationSeconds),
+                    EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+                };
+
+                Storyboard storyboard = new Storyboard();
+                storyboard.Children.Add(colorAnimation);
+
+                Storyboard.SetTarget(storyboard, a);
+                Storyboard.SetTargetProperty(colorAnimation, new PropertyPath("(Rectangle.Fill).(SolidColorBrush.Color)"));
+
+
+                storyboard.Begin(this);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Page1 page1 = new Page1();
+            Panel.Navigate(page1);
+        }
+        
+        void Load(object sender, RoutedEventArgs e)
+        {
+            if (brcl_file.Exists == false)
+            {
+                Directory.CreateDirectory(brcl_file.FullName);
+            }
+            if (op_file.Exists == false)
+            {
+                File.WriteAllText(op_file.FullName, "6000" + "\n" + "1920" + "\n" +"1080");
+            }
         }
     }
 }
---
+++ Page1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Page1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        static string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1];
+        static FileInfo op_file = new FileInfo("C:\\Users\\" + username + "\\AppData\\Roaming\\brcl\\option.txt");
+        public Page1()
+        {
+            InitializeComponent();
+            this.Loaded += Load;
+        }
+
+        private void Height_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            resolution.Content = Height.Value.ToString() + " x " + Width.Value.ToString();
+        }
+
+        private void Width_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            resolution.Content = Height.Value.ToString() + " x " + Width.Value.ToString();
+        }
+
+        void Load(object sender, RoutedEventArgs e)
+        {
+            string[] startData = File.ReadAllLines(op_file.FullName);
+            RamSlider.Value = Int32.Parse(startData[0]);
+
+            Height.Value = Int32.Parse(startData[1]);
+            Width.Value = Int32.Parse(startData[2]);
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(op_file.FullName, RamSlider.Value.ToString() + "\n" + Height.Value.ToString() + "\n" + Width.Value.ToString());
+        }
+
+        private void Ram_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            RamText.Content = RamSlider.Value.ToString() + "MB";
+        }
+    }
+}
